55a0f0d83ea153803db0e507776799eaa90d44b3
sreeja1031 <sreeja.nandyala@zemosolabs.com>
2022-02-10 07:06:13 +0530

    Removed folder from repo

--- src/_components/reduxExample/store.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './counterSlice';
-
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
