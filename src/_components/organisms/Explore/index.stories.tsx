d265d8e3eb3dd06ea8a6f22ee1c1533bf5665865
sreeja1031 <sreeja.nandyala@zemosolabs.com>
2022-02-11 16:26:53 +0530

    fixed error

--- src/_components/organisms/Explore/index.stories.tsx
+++ src/_components/organisms/Explore/index.stories.tsx
@@ -1,9 +1,16 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/function-component-definition */
 import * as React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import ExploreMenu from ".";
 
 export default {
   title: "Components/Organisms",
   component: ExploreMenu,
-};
+} as ComponentMeta<typeof ExploreMenu>;
 
-export const Explorer = <ExploreMenu />;
+const Template: ComponentStory<typeof ExploreMenu> = (args) => (
+  <ExploreMenu {...args} />
+);
+
+export const Explorer = Template.bind({});
