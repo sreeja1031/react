f11be3ca15c6bea4d96ab2c3be8c2fffbb36b9d9
sreeja1031 <sreeja.nandyala@zemosolabs.com>
2022-03-05 05:48:40 +0530

    syntax changes

--- src/_components/atoms/MoreHorizIcon/index.tsx
+++ src/_components/atoms/MoreHorizIcon/index.tsx
@@ -1,20 +1,21 @@
+/* eslint-disable react/function-component-definition */
 import React from "react";
 import MoreHoriz from "@mui/icons-material/MoreHorizOutlined";
 import IconButton from "@mui/material/IconButton";
 
-export default function More() {
-  return (
-    <IconButton
-      aria-label="settings"
-      sx={{
-        left: "85.75%",
-        right: "12.5%",
-        bottom: "41.67%",
-        top: "41.67%",
-        color: "#03314B",
-      }}
-    >
-      <MoreHoriz />
-    </IconButton>
-  );
-}
+const MoreIcon: React.FC = () => (
+  <IconButton
+    aria-label="settings"
+    sx={{
+      left: "85.75%",
+      right: "12.5%",
+      bottom: "41.67%",
+      top: "41.67%",
+      color: "#03314B",
+    }}
+  >
+    <MoreHoriz />
+  </IconButton>
+);
+
+export default MoreIcon;
--- src/_components/pages/BookDetailsPage.tsx
+++ src/_components/pages/BookDetailsPage.tsx
@@ -1,7 +1,14 @@
 /* eslint-disable react/function-component-definition */
 import React from "react";
+import { Grid } from "@mui/material";
 import BookDetails from "../organisms/BookDetails";
+import FooterComponent from "../organisms/Footer";
 
-const BookDetailsPage: React.FC = () => <BookDetails />;
+const BookDetailsPage: React.FC = () => (
+  <>
+    <BookDetails />
+    <FooterComponent />
+  </>
+);
 
 export default BookDetailsPage;
